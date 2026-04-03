a1ff6d82556825d2b7c286052e3788d048d570ff
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2026-04-04 00:37:08 +0200

    Fix imports and add @ alias for src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header/header.jsx"
-import Hero from "./sections/Hero/Hero.jsx"
+import Header from "@/components/Header/Header.jsx";
+import Hero from "@/sections/Hero/Hero.jsx"
+import Projects from "@/sections/Projects/Projects.jsx"
 /* import About from "./sections/About" */
-import Projects from "./sections/Projects/Projects.jsx"
 /* import Skills from "./sections/Skills" */
 /* import Contact from "./sections/Contact" */
 
